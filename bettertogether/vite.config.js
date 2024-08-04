905020988ee3cc19bccae172db34cf2299d532e5
Noviero <78156339+nov1ero@users.noreply.github.com>
2024-08-04 15:55:57 +0600

    add: deploy workflow

--- bettertogether/vite.config.js
+++ bettertogether/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bettertogether/",
   define: {
     global: "globalThis",
     "process.env": {},
