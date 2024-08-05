8115d81004190e43c5f144f96f0fc86af90fb560
Noviero <78156339+nov1ero@users.noreply.github.com>
2024-08-06 05:10:50 +0600

    Deploy: build fixes

--- bettertogether/vite.config.js
+++ bettertogether/vite.config.js
@@ -1,12 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodeResolve()],
   base: "/bettertogether/", // Ensure this matches your repo name
   define: {
     global: "globalThis",
     "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+      external: ['@emailjs/browser'],
+    },
   },
 });
